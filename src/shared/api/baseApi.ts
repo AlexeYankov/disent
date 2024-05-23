bdfb25f657a6e9fa65f3ba4270b7c0b68be286c7
Alexey Yankov <yankovav.wm@gmail.com>
2024-05-23 20:30:19 +0300

    trash fix

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -2,6 +2,4 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_PUBLICK_URL,
-    withCredentials: true,
-    headers: {'x-auth-skip': true},
   });
